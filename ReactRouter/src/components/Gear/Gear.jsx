4b527603b591edc20374343e682e8bfa55e21fd4
codecounter <fjohnwilmar@hotmail.com>
2024-02-10 20:06:01 +0800

    modified:   ReactRouter/src/components/Gear/Gear.jsx 	modified:   ReactRouter/src/components/Gear/gear.css 	modified:   ReactRouter/src/pages/Work/Work.jsx

--- ReactRouter/src/components/Gear/Gear.jsx
+++ ReactRouter/src/components/Gear/Gear.jsx
@@ -5,7 +5,7 @@ const Gear = () => {
   return (
     <div className="gear">
         <img src={gear}/>
-        <h2>Area under development. Check back soon!</h2>
+        <h2>This page is currently under development. <br/>  <br/> If you need to reach me, please feel free to email me at imcodecounter@gmail.com.<br/>  <br/> Thank you </h2>
     </div>
   )
 }
--- ReactRouter/src/pages/Work/Work.jsx
+++ ReactRouter/src/pages/Work/Work.jsx
@@ -1,16 +1,11 @@
 import './work.css'
-// import Gear from '../../components/Gear/Gear'
+import Gear from '../../components/Gear/Gear'
 
 
 const Work = () => {
   return (
     <div className="work">
-      <div className="main_work">
-        <div className="work_containter">
-          
-        </div>
-      </div>
-      {/* <Gear/> */}
+      <Gear/>
     </div>
   )
 }
